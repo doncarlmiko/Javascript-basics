67759e346e1595f1628f578034696369c07cb165
doncarlmiko <konoha11061997@gmail.com>
2024-11-02 15:23:19 +0800

    New changes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 console.log("Hello, World!")
-
+        const name2 = "Steph"
         let name = "John"
         let surname = "Doe"
+        let a = 10
+        let b = 7 * a
+        
 
         console.log(name)
         console.log(surname)
-
-        const name2 = "Steph"
-
         console.log(name2)
+        console.log((4 + 6 + 9) / 77)
+        console.log(a)
+        
+        console.log(9*a)
+        console.log(b)
+       
