e360829d2c0981bc347daf86598056cbf9db2c56
doncarlmiko <konoha11061997@gmail.com>
2024-11-07 21:52:55 +0800

    Save changes

---
+++ js/string task.js
@@ -0,0 +1,9 @@
+const VISITOR = prompt(`What's the “official” name of JavaScript?`,'');
+
+
+if(VISITOR == 'ECMAScript'){
+    alert('Right!');
+}
+else{
+    alert(`You don't know? “ECMAScript”!`);
+}
