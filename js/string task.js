1fc6ad51861c7df2dc6d005fa6e52923c1b518ac
doncarlmiko <konoha11061997@gmail.com>
2024-11-09 14:43:45 +0800

    save string task changes

--- js/string task.js
+++ js/string task.js
@@ -15,20 +15,46 @@ do{
 
     if(inputNumber > 0){
         alert(`greater than 0!`);
+        console.log(inputNumber);
     }
     else if(inputNumber < 0){
         alert(`less than 0!`);
+        console.log(inputNumber);
     }
     
     else if(inputNumber == 0){
         alert(`equal to zero!`);
-        console.log(inputNumber);
     }
     else if (isNaN(inputNumber)==true || null){
         alert('Please enter a number!');
+        console.log(inputNumber);
     }
 
 }while(isNaN(inputNumber)==true);
 
+let result = (inputNumber + 80 < 4) ? 'Below' : 'Over';
+console.log(result);
 
+if (12 + 80 < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+let login = 'Director';
+let message = (login == 'Employee') ? 'Hello'
+? (login == 'Director') :'Greetings'
+? (login == '') : 'No login' : '';
+
+console.log(login);
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
 
