3cc68b013d22d2d3187b418d35e65410c61bb4ed
doncarlmiko <konoha11061997@gmail.com>
2024-11-23 15:49:58 +0800

    push changes

--- js/min function task.js
+++ js/min function task.js
@@ -1,23 +1,47 @@
-let textBox2 = document.querySelector('#textBox2');
+const textBox2 = document.querySelector('#textBox2');
 let output2 = document.querySelector('#output2');
-let textBox3 = document.querySelector('#textBox3');
+
+const textBox3 = document.querySelector('#textBox3');
 let output3= document.querySelector('#output3');
+
+const textBox4 = document.querySelector('#textBox4');
+let output4 = document.querySelector('#output4');
+
+const textBox5 = document.querySelector('#textBox5');
+let output5= document.querySelector('#output5');
 //const passOutput = displayOutput(output2);
 
-function min(a, b){
-    const getMin = (a < b) ? a : b ;
-    return getMin;
+
+const min = (a, b) =>{
+    const getMin = (a < b) ? a : b;
+    const getMax = (a > b) ? a : b;
+   return {min: getMin, max: getMax };
 }
 
-function calcMinAndMax (){
-    const input1 = textBox2.value;
-    const input2 = textBox3.value;
-    const result = min(input1,input2);
+const calcMinAndMax =(inputbox1,inputbox2,outputBox1,outputBox2) =>{
+    const input1 = Number(inputbox1.value);
+    const input2 = Number(inputbox2.value);
+    const {min: passMin, max: passMax } = min(input1,input2);
+    
+   // output2.textContent = `Minimum number is ${passMin}`;
+    //output3.textContent = `Maximum number is ${passMax}`;
 
-    const outputMin = ( result > input1 ) ? output2.textContent= `This is the minimum number` : output3.textContent= `This is the maximum number`;
+    if(passMin == input1 && passMax == input2){
+        outputBox1.textContent =`Minimum number ${passMin}`;
+        outputBox2.textContent = `Maximum number ${passMax}`;
+    }
+    else {
+        outputBox1.textContent = `Maximum number`;
+        outputBox2.textContent = `Minimum number`;
+    }
+    
+    //output2.textContent = passMin;
+    //output3.textContent = passMin;
 }
 
 //const passNumber = min(30, 12);
 //console.log(`The minimum number is ${passNumber}`);
-textBox2.addEventListener('input', calcMinAndMax);
-textBox3.addEventListener('input', calcMinAndMax);
+textBox2.addEventListener('input', ()=>calcMinAndMax(textBox2,textBox3,output2,output3));
+textBox3.addEventListener('input', ()=>calcMinAndMax(textBox2,textBox3,output2,output3));
+textBox4.addEventListener('input', ()=>calcMinAndMax(textBox4,textBox5,output4,output5));
+textBox5.addEventListener('input', ()=>calcMinAndMax(textBox4,textBox5,output4,output5));
