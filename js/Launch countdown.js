f53666875d9d87e193906fe14ac20b432cf78f86
doncarlmiko <konoha11061997@gmail.com>
2024-12-15 16:42:16 +0800

    modified the pseudocode and fix the typeerror on output.appendChild by adding the defer to the script tag

--- js/Launch countdown.js
+++ js/Launch countdown.js
@@ -1,1 +1,20 @@
 const output = document.querySelector('.output');
+
+
+
+for(let i=10; i>=0; i--){
+    const para = document.createElement('p');
+
+    if(i === 10){
+        para.textContent=`Countdown ${i}`;
+    }
+    else if(i=== 0){
+        para.textContent= "BLAST OFF!";
+    }
+    else{
+        
+    para.textContent = i;
+    }
+    output.appendChild(para);
+}
+
