407c40c35325ec60c09e21cfd69fefcdc8362458
doncarlmiko <konoha11061997@gmail.com>
2025-01-25 18:28:25 +0800

    added a style color for redParagraphText and blueHeaderText

---
+++ js/dom_exercise.js
@@ -0,0 +1,10 @@
+const redParagraphText = document.querySelector(".red");
+const blueHeaderText = document.querySelector(".blue");
+const container = document.querySelector(".container");
+
+const headerFirstchild = document.createElement("h1");
+const paragraphSibling = document.createElement("p");
+
+//change color for classes 
+redParagraphText.style.color = "red";
+blueHeaderText.style.color = "blue";
