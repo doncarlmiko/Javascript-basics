0d0913c2acd5dc14dd5a3b87ed5e3556a039174d
doncarlmiko <konoha11061997@gmail.com>
2025-01-25 19:02:23 +0800

    Added styles for my class container div

--- js/dom_exercise.js
+++ js/dom_exercise.js
@@ -8,3 +8,15 @@ const paragraphSibling = document.createElement("p");
 //change color for classes 
 redParagraphText.style.color = "red";
 blueHeaderText.style.color = "blue";
+
+//container.style.cssText = "color: blue; background: white;";
+container.style["backgroundColor"] = "pink";
+container.style.border = "5px solid black";
+container.style["borderColor"] = "black";
+
+headerFirstchild.textContent="I’m in a div";
+paragraphSibling.textContent ="ME TOO!";
+
+container.appendChild(headerFirstchild);
+container.appendChild(paragraphSibling);
+
