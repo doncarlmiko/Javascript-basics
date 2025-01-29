82612fec5f8b89a2c072bf695811867f5379c70f
doncarlmiko <konoha11061997@gmail.com>
2025-01-29 15:39:44 +0800

    Added functions for deleting all items on the list

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,25 +1,54 @@
 const userInput = document.querySelector('#item');
 const addItemButton = document.querySelector('button');
 const shoppingList = document.querySelector('ul');
+const deleteAllList = document.querySelector('#DeleteAll');
 
 //userInput.value;
 addItemButton.addEventListener('click',getUserInput);
+deleteAllList.addEventListener('click',deleteAllItems);
 
 
 function getUserInput(){
     const itemList = document.createElement('li');
     const listName = document.createElement('span');
     const deleteList= document.createElement('button');
+    //const deleteAllList= document.createElement('button');
 
-    listName.textContent =userInput.value;
-    deleteList.textContent='Delete';
-    //itemList.textContent = `${listName.value}${deleteList.value}`;
+    if(userInput.value != ''){
+        listName.textContent =userInput.value;
+        deleteList.textContent='Delete';
+        
+        deleteAllList.style.display='block';
+        itemList.setAttribute("class", "highlight");
 
-    shoppingList.appendChild(itemList);
-    itemList.appendChild(listName);
-    itemList.appendChild(deleteList);
 
-    userInput.value='';
+        shoppingList.appendChild(itemList);
+        itemList.appendChild(listName);
+        itemList.appendChild(deleteList);
 
-    deleteList.addEventListener('click', ()=> shoppingList.removeChild(itemList));
+        userInput.value='';
+
+        deleteList.addEventListener('click', ()=> {
+            shoppingList.removeChild(itemList);
+            checkIfEmpty();
+
+        });
+    }
+
+    else{
+        alert('Please input an item!');
+    }
+    
 }
+
+function deleteAllItems(){
+    shoppingList.textContent = '';
+    deleteAllList.style.display='none';
+}
+
+function checkIfEmpty(){
+    if(shoppingList.children.length === 0){
+        deleteAllList.style.display='none';
+    }
+}
+
