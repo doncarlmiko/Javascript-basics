f0ddb8194c7ceab899d52a44ac55e02bcd05fb4b
doncarlmiko <konoha11061997@gmail.com>
2025-04-26 00:29:53 +0800

    Finished task 16 - create keyed objects from array

---
+++ js/Objects and Arrays/task 16 - create keyed objects from array.js
@@ -0,0 +1,19 @@
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+  ];
+
+
+  function groupById(arr){
+    let objectId = {};
+
+    arr.forEach(element => {
+        objectId[element.id] = element;
+    });
+    return objectId;
+  }
+  
+  let usersById = groupById(users);
+
+  console.log(usersById.john); // { john
